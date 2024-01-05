541a64c35b7c300b47f023d1296ad262d34e361b
halocean96 <halocean96@gmail.com>
2024-01-05 20:24:16 +0900

    Setup: radix +  prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
+			<App />
+			<ThemePanel />
+		</Theme>
+	</React.StrictMode>,
+);
