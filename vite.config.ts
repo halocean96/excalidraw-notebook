903049d24e710320a253b45ac596e45e73c60cbb
halocean96 <halocean96@gmail.com>
2024-01-05 23:14:36 +0900

    Docs: why define process.env

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	define: {
+		// excalidraw reffers to this variable to check if it's running in preact
 		'process.env.IS_PREACT': JSON.stringify('false')
 	}
 });
