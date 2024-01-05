9d6a0fce8f80dd50124a8f2b1fb4cac814851113
halocean96 <halocean96@gmail.com>
2024-01-05 21:22:01 +0900

    Update: prettierrc

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { Theme, ThemePanel } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
+		<Theme
+			accentColor="crimson"
+			grayColor="sand"
+			radius="large"
+			scaling="95%"
+		>
 			<App />
 			<ThemePanel />
 		</Theme>
-	</React.StrictMode>,
+	</React.StrictMode>
 );
