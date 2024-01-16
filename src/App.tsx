a33645f9daa46e0dc3612b7c51fd17d74d6a91ba
halocean96 <halocean96@gmail.com>
2024-01-16 23:35:58 +0900

    Update: 모바일 대응

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import { Flex } from '@radix-ui/themes';
-import { Excalidraw } from '@excalidraw/excalidraw';
+import { Box, Flex } from '@radix-ui/themes';
+import { Excalidraw, MainMenu } from '@excalidraw/excalidraw';
 import { throttle } from 'radash';
 import { useNoteStore } from './store';
 import View from './View';
 import { useCallback } from 'react';
+import { isMobile } from './utils';
 
 function App() {
 	const { setExcalidrawAPI, onNoteChange } = useNoteStore((state) => ({
 		setExcalidrawAPI: state.setExcalidrawApi,
 		onNoteChange: state.onNoteChange
 	}));
-
 	const onChangeThrottle = useCallback(
 		throttle({ interval: 500 }, onNoteChange),
 		[onNoteChange]
 	);
 	return (
 		<Flex direction={'row'} className="w-screen h-screen">
-			<View />
+			<Box className="max-w-64 hidden lg:block">
+				<View />
+			</Box>
 			<Flex justify={'center'} align={'center'} grow={'1'}>
 				<Excalidraw
 					onChange={onChangeThrottle}
 					excalidrawAPI={(api) => setExcalidrawAPI(api)}
-				/>
+				>
+					{isMobile() ? (
+						<MainMenu>
+							<View />
+						</MainMenu>
+					) : null}
+				</Excalidraw>
 			</Flex>
 		</Flex>
 	);
